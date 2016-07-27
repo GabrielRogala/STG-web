de06c49b59e34e06483d91de5d259b26b181f266
GabrielRogala <gabriel.rogala@gmail.com>
2016-07-27 13:21:03 +0200

    czysty projekt

---
+++ STG web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(STG_web.Startup))]
+namespace STG_web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
